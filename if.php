12de794533cb82ab3708ef3eafc453768fdc2d37
C2RUN <jose.cerda66@gmail.com>
2015-12-17 14:34:54 -0300

    Primeros cambios
    Ejemplo de bucle IF y elseif en php

---
+++ if.php
@@ -0,0 +1,18 @@
+<?php
+// IF ejemplo
+ $x = 30;
+
+ if($x == 10)
+ {
+ echo "El valor de x es 10";
+ }
+ elseif($x == 20)
+ {
+ echo "El valor de x es 20";
+ }
+ else
+ {
+ echo "El valor de x es 30";
+ }
+
+?>
